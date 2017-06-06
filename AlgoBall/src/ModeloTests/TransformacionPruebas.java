051ca43a684f3cbd5114b7663cc67237b374283e
tomiir <rocchitomas@gmail.com>
2017-06-06 02:37:35 -0300

    Test05 OK

--- AlgoBall/src/ModeloTests/TransformacionPruebas.java
+++ AlgoBall/src/ModeloTests/TransformacionPruebas.java
@@ -7,6 +7,8 @@ import org.junit.Assert;
 
 import org.junit.Test;
 
+import Modelo.Direccion;
+import Modelo.ExcDireccionInvalida;
 import Modelo.ExcFueraDeTablero;
 import Modelo.ExcNoEsPosibleTransformarse;
 import Modelo.ExcPosicionOcupada;
@@ -14,13 +16,15 @@ import Modelo.ExcPosicionOcupada;
 
 public class TransformacionPruebas {
 	
-	Tablero tablero = new Tablero(15,15);
-	Goku goku = new Goku(tablero);
+	
 	
 	
 	@Test
 	public void Test04SePosicionaYTransformaCorrectamente() throws ExcNoEsPosibleTransformarse, ExcPosicionOcupada, ExcFueraDeTablero{
+		Tablero tablero = new Tablero(15,15);
+		Goku goku = new Goku(tablero);
 		Posicion posicion =  new Posicion(10,10);
+		
 		tablero.posicionar(goku, posicion);
 		Assert.assertEquals(goku.posicion(), posicion);
 		
@@ -33,4 +37,31 @@ public class TransformacionPruebas {
 		Assert.assertEquals(goku.ki(), 80);
 		
 	}
+	 @Test
+	 public void Test05TransformaYMueveCorrectamente() throws ExcPosicionOcupada, ExcFueraDeTablero, ExcDireccionInvalida, ExcNoEsPosibleTransformarse{
+		 Tablero tablero = new Tablero(15,15);
+		 Goku goku = new Goku(tablero);
+		 Posicion posicion =  new Posicion(10,10);
+			
+		 tablero.posicionar(goku, posicion);
+		 Assert.assertEquals(goku.posicion(), posicion);
+		goku.incrementarKi(100);
+		
+		goku.transformar();
+		Assert.assertEquals(goku.velocidad(), 3);
+		Assert.assertEquals(goku.rangoDeAtaque(), 4);
+		Assert.assertEquals(goku.getDmgNormal(), 40);
+		Assert.assertEquals(goku.ki(), 80);
+		
+		goku.mover(new Direccion(1,0));
+		Assert.assertEquals(goku.posicion().posX(), 11);
+		Assert.assertEquals(goku.posicion().posY(), 10);
+		
+		
+		
+		 
+		 
+		 
+		 
+	 }
 }
