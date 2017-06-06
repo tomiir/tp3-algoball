95951c89380440ba37a5f158bb90befcb872e4b0
tomiir <rocchitomas@gmail.com>
2017-06-06 02:54:28 -0300

    edit

--- AlgoBall/src/Modelo/Juego.java
+++ AlgoBall/src/Modelo/Juego.java
@@ -1,5 +1,14 @@
 package Modelo;
 
-public class Juego {
+import java.util.ArrayList;
 
+public class Juego {
+	Tablero tablero;
+	ArrayList<Personaje> personajes;
+	
+	public Juego(int sizex, int sizey){
+		tablero = new Tablero(sizex,sizey);
+		personajes = new ArrayList<Personaje>();
+	}
+	
 }
--- AlgoBall/src/ModeloTests/IntegracionPruebas.java
+++ AlgoBall/src/ModeloTests/IntegracionPruebas.java
@@ -1,5 +1,21 @@
 package ModeloTests;
 
+import Modelo.Tablero;
+import Modelo.Goku;
+import Modelo.Posicion;
+import org.junit.Assert;
+
+import org.junit.Test;
+
+import Modelo.Direccion;
+import Modelo.ExcDireccionInvalida;
+import Modelo.ExcFueraDeTablero;
+import Modelo.ExcNoEsPosibleTransformarse;
+import Modelo.ExcPosicionOcupada;
+
+
 public class IntegracionPruebas {
+	
+	
 
 }
