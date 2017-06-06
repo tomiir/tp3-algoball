9282424c1e97ef0eb60fa1e861205685bb72237c
urielkelman <urielkelman@gmail.com>
2017-06-06 15:57:41 -0300

    edit pruebas 2

--- AlgoBall/src/ModeloTests/IntegracionPruebas.java
+++ AlgoBall/src/ModeloTests/IntegracionPruebas.java
@@ -1,9 +1,17 @@
 package ModeloTests;
+import Modelo.Jugador;
 
 import Modelo.Tablero;
 import Modelo.Goku;
 import Modelo.Posicion;
 import org.junit.Assert;
+import Modelo.Equipo;
+import Modelo.Gohan;
+import Modelo.Piccolo;
+import Modelo.Freezer;
+import Modelo.Cell;
+import Modelo.MajinBoo;
+import Modelo.Personaje;
 
 import org.junit.Test;
 
@@ -14,26 +22,58 @@ import Modelo.ExcNoEsPosibleTransformarse;
 import Modelo.ExcPosicionOcupada;
 import Modelo.Juego;
 
+
 public class IntegracionPruebas {
 	
-	Gohan gohan = new Gohan();
-	Piccolo piccolo = new Piccolo();
-	Goku goku = new Goku();
 	
-	Cell cell = new Cell();
-	Freezer freezer = new Freezer();
-	MajinBoo majinBoo = new MajinBoo();
+	Jugador jugador1 = new Jugador("J1");
+	Jugador jugador2 = new Jugador("J2");
 	
-	Equipo equipo = new Equipo([gohan,goku,piccolo]);
+	Tablero tablero = new Tablero(20,20);
+	
+	Goku goku = new Goku(tablero);
+	Gohan gohan = new Gohan(tablero);
+	Piccolo piccolo = new Piccolo(tablero);
+	
+
+	Cell cell = new Cell(tablero);
+	Freezer freezer = new Freezer(tablero);
+	MajinBoo majinBoo = new MajinBoo(tablero);
 	
 	
 	@Test
-	public void crearNuevoJuego(){
+	public void Test06CrearJugadoresYPosicionarEquipos() throws ExcPosicionOcupada, ExcFueraDeTablero{
+	
+		Equipo guerrerosZ = new Equipo("Guerreros Z");
 		
-		Juego juego = new Juego(15,15);
+		guerrerosZ.agregarPersonaje(goku);
+		guerrerosZ.agregarPersonaje(gohan);
+		guerrerosZ.agregarPersonaje(piccolo);
 		
 		
+		Equipo enemigos = new Equipo("Enemigos de la Tierra");
+		
+		enemigos.agregarPersonaje(cell);
+		enemigos.agregarPersonaje(freezer);
+		enemigos.agregarPersonaje(majinBoo);
+		
+		jugador1.asignarEquipo(guerrerosZ);
+		jugador2.asignarEquipo(enemigos);
+		
+		
+		tablero.posicionar(goku,new Posicion(1,0));
+		tablero.posicionar(gohan,new Posicion(2,0));
+		tablero.posicionar(piccolo,new Posicion(3,0));
+		
+		tablero.posicionar(freezer,  new Posicion(20,20));
+		tablero.posicionar(cell, new Posicion(19,20));
+		tablero.posicionar(majinBoo, new Posicion(18,20));
+		
+		
+	
 	}
+	
+	
 	
 
 }
