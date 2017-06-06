df45223cac2bc32b7e77b44894c9981fcf7c7229
tomiir <rocchitomas@gmail.com>
2017-06-06 00:29:38 -0300

    Restauracion tablero

---
+++ AlgoBall/src/Modelo/Direccion.java
@@ -0,0 +1,10 @@
+package Modelo;
+
+public class Direccion {	
+	int dx;
+	int dy;
+}
+
+
+
+
--- AlgoBall/src/Modelo/Tablero.java
+++ AlgoBall/src/Modelo/Tablero.java
@@ -15,27 +15,14 @@ public class Tablero {
 		
 	}
 	
-<<<<<<< HEAD
-	public void posicionar(Posicionable posicionable, Posicion posicion) throws ExcCasilleroOcupado, ExcFueraDeTablero{
-		if(!coordenadasEstanEnRango(posicion)) throw new ExcFueraDeTablero();
-		Casillero casillero = casilleros.get(posicion);
-		posicionable.posicionar(casillero);
-	}
-	
 	public Casillero obtenerCasillero(int posX,int posY) throws ExcFueraDeTablero{
 		if(!coordenadasEstanEnRango(posicion)) throw new ExcFueraDeTablero();
 		return casilleros.get(posicion);
 	}
-	
-	private boolean coordenadasEstanEnRango(Posicion posicion) {
-		posX = posicion.posX();
-		posY = posicion.posY();
-				
-		return (posX >= 0 && posX < ancho && posY >= 0 && posY < alto);
-=======
+
 	public void posicionar(Posicionable posicionable, Posicion pos) throws ExcCasilleroOcupado, ExcFueraDeTablero{
 		if(!coordenadasEstanEnRango(pos)) throw new ExcFueraDeTablero();
-		Casillero casillero = casilleros[pos.posX()][pos.posY()];
+		Casillero casillero = casilleros.get(pos);
 		try{
 			casillero.posicionar(posicionable);
 		}	catch(ExcCasilleroOcupado e){
@@ -46,6 +33,5 @@ public class Tablero {
 	
 	private boolean coordenadasEstanEnRango(Posicion pos) {
 		return (pos.posX() >= 0 && pos.posX() < ancho && pos.posY() >= 0 && pos.posY() < alto);
->>>>>>> fa6f773502b781b0003017067e2bcda5d3848684
 	}
 }
