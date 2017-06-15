e81e7d63a8d1e378ace8bec66db870dfa22118e0
urielkelman <urielkelman@gmail.com>
2017-06-15 15:41:12 -0300

    Se empezaron a corregir las pruebas de la segunda entrega

--- AlgoBall/src/ModeloTests/Entregas/Segunda/test01.java
+++ AlgoBall/src/ModeloTests/Entregas/Segunda/test01.java
@@ -5,9 +5,18 @@ import org.junit.Test;
 import Modelo.Equipo;
 import Modelo.Jugador;
 import Modelo.Partida;
+import Modelo.Posicion;
 import Modelo.Tablero;
+import Modelo.Excepciones.ExcCasilleroOcupado;
+import Modelo.Excepciones.ExcEsChocolate;
+import Modelo.Excepciones.ExcFueraDeRango;
+import Modelo.Excepciones.ExcFueraDeTablero;
 import Modelo.Excepciones.ExcHayGanador;
+import Modelo.Excepciones.ExcKiInsuficiente;
 import Modelo.Excepciones.ExcNoEsPosibleTransformarse;
+import Modelo.Excepciones.ExcNumeroNegativo;
+import Modelo.Excepciones.ExcPersonajeMurio;
+import Modelo.Excepciones.ExcPosicionNegativa;
 import Modelo.Personajes.Cell;
 import Modelo.Personajes.Freezer;
 import Modelo.Personajes.Gohan;
@@ -18,86 +27,64 @@ import org.junit.Assert;
 
 public class test01 {
 	Tablero tablero = new Tablero(15, 15);
-	Jugador jugador1 = new Jugador("Jugador Guerreros Z");
-	Jugador jugador2 = new Jugador("Jugador Enemigos de la tierra");
-	Partida partida;
-	Equipo equipo1;
-	Equipo equipo2;
-	Goku goku;
-	Gohan gohan;
-	Piccolo piccolo;
-	Cell cell;
-	Freezer freezer;
-	MajinBoo majinBoo;
-	
-	
-	
-	private void iniciarPartida(){
-		partida = new Partida(tablero, jugador1, jugador2);
-		equipo1 = new Equipo("Guerreros Z");
-		equipo2 = new Equipo("Enemigos de la tierra");
-		goku = new Goku(partida);
-		gohan = new Gohan(partida);
-		piccolo = new Piccolo(partida);
-		cell = new Cell(partida);
-		freezer = new Freezer(partida);
-		majinBoo = new MajinBoo(partida);
-		
-		//Guerreros Z
-		equipo1.agregarPersonaje (goku);
-		equipo1.agregarPersonaje (gohan);
-		equipo1.agregarPersonaje (piccolo);
-		
-		//Enemigos de la tierra
-		equipo2.agregarPersonaje (cell);
-		equipo2.agregarPersonaje (freezer);
-		equipo2.agregarPersonaje (majinBoo);
-		
-		jugador1.asignarEquipo(equipo1);
-		jugador2.asignarEquipo(equipo2);
-		partida.iniciar();
-	}
-	
-	@Test (expected = ExcNoEsPosibleTransformarse.class)
-	public void GohanNoSePuedeTransformar() throws ExcNoEsPosibleTransformarse{
-		iniciarPartida();
-		gohan.transformar();
-	}
+	Equipo GuerrerosZ = new Equipo("GuerrerosZ");
+	Goku goku = new Goku(tablero);
+	Piccolo piccolo = new Piccolo(tablero);
+	Gohan gohan = new Gohan(tablero);
 	
 	@Test
-	public void GohanSePuedeTransformarPasados2Turnos() throws ExcNoEsPosibleTransformarse, ExcHayGanador{
-		iniciarPartida();
-		partida.avanzarTurno();
-		Assert.assertEquals("El ki de gohan es correcto", gohan.ki(), 10);
-		gohan.transformar();
-		Assert.assertEquals("El ki de gohan es correcto", gohan.ki(), 0);
-		Assert.assertEquals("El poder de pelea de gohan es correcto", gohan.poderDePelea(),30);
-		Assert.assertEquals("El rango de ataque de gohan es correcto", gohan.rangoDeAtaque(), 2);
-		Assert.assertEquals("La velocidad de gohan es correcta", gohan.velocidad(), 2);
+	public void GohanSeTransformaCorrectamente() throws ExcNoEsPosibleTransformarse, ExcFueraDeTablero, ExcCasilleroOcupado, ExcPosicionNegativa, ExcFueraDeRango, ExcKiInsuficiente, ExcPersonajeMurio, ExcEsChocolate, ExcNumeroNegativo{
+		GuerrerosZ.agregarPersonaje(goku);
+		GuerrerosZ.agregarPersonaje(piccolo);
+		GuerrerosZ.agregarPersonaje(gohan);
+		
+		Equipo EnemigosDeLaTierra = new Equipo("EnemigosDeLaTierra");
+		Freezer freezer = new Freezer(tablero);
+		EnemigosDeLaTierra.agregarPersonaje(freezer);
+		
+		tablero.posicionarPersonaje(goku, new Posicion(1, 1));
+		tablero.posicionarPersonaje(piccolo, new Posicion(2, 2));
+		tablero.posicionarPersonaje(gohan, new Posicion(5, 5));
+		
+		tablero.posicionarPersonaje(freezer, new Posicion(6, 6));;
+		
+		int vidaInicialFreezer = freezer.puntosDeVida();
+		gohan.atacar(freezer, false);
+		
+		int vidaTrasPrimerAtaque = freezer.puntosDeVida();
+		
+		int dañoRealizadoSinTransformar = vidaInicialFreezer - vidaTrasPrimerAtaque;
+		
+		gohan.seAvanzoUnTurno(10);
+		
+		gohan.transformar(GuerrerosZ);
+		
+		gohan.atacar(freezer, false);
+		
+		int vidaTrasSegundoAtaque = freezer.puntosDeVida();
+		
+		int dañoRealizadoSiTransformo = vidaTrasPrimerAtaque - vidaTrasSegundoAtaque;
+		
+		Assert.assertTrue("El ataque cuando se transformo saca mas vida", (dañoRealizadoSiTransformo - dañoRealizadoSinTransformar) > 0);
+		
 	}
 	
 	@Test (expected = ExcNoEsPosibleTransformarse.class)
-	public void GohanNoSePuedeTransformarDevuelta() throws ExcNoEsPosibleTransformarse, ExcHayGanador{
-		iniciarPartida();
-		partida.avanzarTurno();
-		Assert.assertEquals("El ki de gohan es correcto", gohan.ki(), 10);
-		gohan.transformar();
-		Assert.assertEquals("El ki de gohan es correcto", gohan.ki(), 0);
-		gohan.transformar();
+	public void GohanNoPuedeLlegarALaSegundaTransformacionYaQueNoSeAtacoASusCompañeros() throws ExcNumeroNegativo, ExcNoEsPosibleTransformarse, ExcEsChocolate{
+		GuerrerosZ.agregarPersonaje(goku);
+		GuerrerosZ.agregarPersonaje(piccolo);
+		GuerrerosZ.agregarPersonaje(gohan);
+		
+		gohan.seAvanzoUnTurno(10);
+		gohan.transformar(GuerrerosZ);
+		
+		gohan.seAvanzoUnTurno(30);
+		gohan.transformar(GuerrerosZ);		
+		
 	}
 	
-	@Test (expected = ExcNoEsPosibleTransformarse.class)
-	public void gohanNoSePuedeTransformarDevueltaConKiSuficiente() throws ExcNoEsPosibleTransformarse, ExcHayGanador{
-		iniciarPartida();
-		partida.avanzarTurno();
-		Assert.assertEquals("El ki de gohan es correcto", gohan.ki(), 10);
-		gohan.transformar();
-		Assert.assertEquals("El ki de gohan es correcto", gohan.ki(), 0);
-		for(int i=0;i<6;i++){
-			partida.avanzarTurno();
-		}
-		Assert.assertEquals("El ki de gohan es correcto", gohan.ki(), 30);
-		gohan.transformar();
-	}
 	
+	
+		
 }
+	
